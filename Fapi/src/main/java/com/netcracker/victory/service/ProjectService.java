92a24d615fed3e27c3eb9509eba863567e3a9886
Вика Киричук <kirichukviktory@gmail.com>
2019-04-16 14:09:26 +0300

    version 13

---
+++ Fapi/src/main/java/com/netcracker/victory/service/ProjectService.java
@@ -0,0 +1,13 @@
+package com.netcracker.victory.service;
+
+import com.netcracker.victory.models.ProjectModel;
+
+import java.util.List;
+
+public interface ProjectService {
+    List<ProjectModel> getAll();
+    ProjectModel getProjectById(int porjectId);
+    ProjectModel getProjectByCode (String projectCode);
+    ProjectModel getProjectByName(String projectName);
+    ProjectModel saveProject(ProjectModel project);
+}
